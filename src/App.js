0b0ecfaa221981f0f6079651a7487e90da5f7e34
jairorjuela <genius85862@gmail.com>
2018-02-05 22:05:09 -0500

    Estado de Componentes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
+import Welcome from './Welcome.js'
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      title: "Hola mundo desde estado"
+    }
+  }
   render() {
+    const names = ["Jairo", "Andres", "Orjuela"]
     return (
-      <h1>Hola Jairo</h1>
+      <div>
+        <h1>{this.state.title}</h1>
+        <button onClick={this.changeTitle.bind(this)}>Cambie el titulo</button>
+      </div>
     );
   }
+
+changeTitle() {
+  this.setState({
+    title: "Nuevo Titulo"
+  })
 }
 
+}
 export default App;
